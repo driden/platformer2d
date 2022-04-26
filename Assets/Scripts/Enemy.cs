7d6ea8baad1a84cf2795de6b9c4cf0ea2a8f9664
driden <776982+driden@users.noreply.github.com>
2022-04-25 21:00:55 -0300

    enemyv2

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,51 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public int Health = 100;
+
+    public float MaxWalkingDistance = 3f;
+    public float MovementSpeed = 2f;
+    public Rigidbody2D rb;
+    private Vector2 StartingPosition;
+
+    private bool IsFacingRight = true;
+
+    void Start()
+    {
+        StartingPosition = this.rb.position;
+    }
+
+    void FixedUpdate()
+    {
+        this.rb.velocity = CalculateVel();
+        if (!CanKeepMoving())
+        {
+            SwitchDirection();
+        }
+    }
+
+    private void SwitchDirection()
+    {
+        Flip();
+        this.rb.velocity = CalculateVel();
+    }
+
+    Vector2 CalculateVel()
+    {
+        return new Vector2((IsFacingRight ? 1 : -1) * this.MovementSpeed, this.rb.velocity.y);
+    }
+
+    private bool CanKeepMoving()
+    {
+        var startX = this.StartingPosition.x;
+        var currX = this.rb.position.x;
+
+        var dX = this.rb.position.x - this.StartingPosition.x;
+        Debug.Log($"[{startX}][{currX}][{dX}]");
+        return Math.Abs(dX) < MaxWalkingDistance;
+    }
 
     public void TakeDamage(int damage)
     {
@@ -17,5 +60,12 @@ public class Enemy : MonoBehaviour
     public void Die()
     {
         Destroy(gameObject);
+    }
+
+    void Flip()
+    {
+        IsFacingRight = !IsFacingRight;
+        Debug.Log(IsFacingRight ? "Right" : "Left");
+        transform.Rotate(0f, 180f, 0f);
     }
 }
